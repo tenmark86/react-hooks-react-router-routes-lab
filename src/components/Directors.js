919246ee014b9f42afacbc48af8583746f8064e3
tenmark86e <tenmark86@gmail.com>
2022-09-25 20:53:10 +0300

    Done Basic Routes Lab assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorDisplay = directors.map((director) => (
+    <div key={director.name}>
+      <h2>Name: {director.name}</h2>
+      Movies:
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorDisplay}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesDisplay = movies.map((movie) => (
+    <div key={movie.title}>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      Genres:
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesDisplay}
+    </div>
+  );
 }
 
 export default Movies;
